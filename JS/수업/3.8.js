74fe15680107f6b9ad6aaf23fbede194347a60c1
김해린 <rin3583@naver.com>
2021-03-08 22:57:15 +0900

    class example

---
+++ JS/수업/3.8.js
@@ -0,0 +1,21 @@
+// let emptyvar;
+
+// let nullVar = null;
+
+// let singleChar = "f";
+
+// console.log(typeof singleChar, typeof emptyvar, typeof nullVar);
+
+// let num = 5 / 2;
+
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
+
+const my = {
+  name: "rin",
+  age: 19,
+  number: 3105,
+};
+
+console.log(my);
