fb4756fdef2d9eb64eeb82826c225f2ebb8b8c12
김해린 <rin3583@naver.com>
2021-03-25 20:08:17 +0900

    next training change

--- Next/Training/practice/pages/about.js
+++
@@ -1,9 +0,0 @@
-import Header from "../components/Header";
-import Layout from "../components/Layout";
-
-export default () => (
-  <Layout>
-    {" "}
-    <p>This is the about page</p>
-  </Layout>
-);
